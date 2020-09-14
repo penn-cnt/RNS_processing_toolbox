9498182d4d99f7985a86f3d1b9eb1a216a90bcfc
Brittany Scheid <bscheid@seas.upenn.edu>
2020-09-14 17:26:07 -0400

    actually save RNS files

---
+++ conversion/parsePDMS_file.m
@@ -0,0 +1,114 @@
+function PDMS_data= parsePDMS_file(pathToCSV, outputFileName)
+% Parses CSV file and saves "PDMS_data.mat" in the same location
+
+[pth,nm]=fileparts(pathToCSV);
+
+T= readtable(pathToCSV);
+
+disp(['parsing', nm])
+
+T.Programming_Date = T.Programming_Date + calyears(2000);
+
+T.Tx1_B1 = parseLeads(T.Tx1_B1);
+T.Tx1_B2 = parseLeads(T.Tx1_B2);
+T.Tx2_B1 = parseLeads(T.Tx2_B1);
+T.Tx2_B2 = parseLeads(T.Tx2_B2);
+T.Tx3_B1 = parseLeads(T.Tx3_B1);
+T.Tx3_B2 = parseLeads(T.Tx3_B2);
+T.Tx4_B1 = parseLeads(T.Tx4_B1);
+T.Tx4_B2 = parseLeads(T.Tx4_B2);
+T.Tx5_B1 = parseLeads(T.Tx5_B1);
+T.Tx5_B2 = parseLeads(T.Tx5_B2);
+
+T.ActiveTrodes= ((T.Tx1_B1~=0)+(T.Tx1_B2 ~=0)+ ...
+    (T.Tx2_B1~=0)+(T.Tx2_B2 ~=0)+ ...
+    (T.Tx3_B1~=0)+(T.Tx3_B2 ~=0)+ ...
+    (T.Tx4_B1~=0)+(T.Tx4_B2 ~=0)+ ...
+    (T.Tx5_B1~=0)+(T.Tx5_B2 ~=0)) ~= 0;
+
+T.Current_Tx1_B1 = extractNumArray(T.Current_Tx1_B1);
+T.Current_Tx1_B2 = extractNumArray(T.Current_Tx1_B2);
+T.Current_Tx2_B1 = extractNumArray(T.Current_Tx2_B1);
+T.Current_Tx2_B2 = extractNumArray(T.Current_Tx2_B2);
+T.Current_Tx3_B1 = extractNumArray(T.Current_Tx3_B1);
+T.Current_Tx3_B2 = extractNumArray(T.Current_Tx3_B2);
+T.Current_Tx4_B1 = extractNumArray(T.Current_Tx4_B1);
+T.Current_Tx4_B2 = extractNumArray(T.Current_Tx4_B2);
+T.Current_Tx5_B1 = extractNumArray(T.Current_Tx5_B1);
+T.Current_Tx5_B2 = extractNumArray(T.Current_Tx5_B2);
+
+T.Freq_Tx1_B1 = extractNumArray(T.Freq_Tx1_B1);
+T.Freq_Tx1_B2 = extractNumArray(T.Freq_Tx1_B2);
+T.Freq_Tx2_B1 = extractNumArray(T.Freq_Tx2_B1);
+T.Freq_Tx2_B2 = extractNumArray(T.Freq_Tx2_B2);
+T.Freq_Tx3_B1 = extractNumArray(T.Freq_Tx3_B1);
+T.Freq_Tx3_B2 = extractNumArray(T.Freq_Tx3_B2);
+T.Freq_Tx4_B1 = extractNumArray(T.Freq_Tx4_B1);
+T.Freq_Tx4_B2 = extractNumArray(T.Freq_Tx4_B2);
+T.Freq_Tx5_B1 = extractNumArray(T.Freq_Tx5_B1);
+T.Freq_Tx5_B2 = extractNumArray(T.Freq_Tx5_B2);
+
+T.PW_Tx1_B1 = extractNumArray(T.PW_Tx1_B1);
+T.PW_Tx1_B2 = extractNumArray(T.PW_Tx1_B2);
+T.PW_Tx2_B1 = extractNumArray(T.PW_Tx2_B1);
+T.PW_Tx2_B2 = extractNumArray(T.PW_Tx2_B2);
+T.PW_Tx3_B1 = extractNumArray(T.PW_Tx3_B1);
+T.PW_Tx3_B2 = extractNumArray(T.PW_Tx3_B2);
+T.PW_Tx4_B1 = extractNumArray(T.PW_Tx4_B1);
+T.PW_Tx4_B2 = extractNumArray(T.PW_Tx4_B2);
+T.PW_Tx5_B1 = extractNumArray(T.PW_Tx5_B1);
+T.PW_Tx5_B2 = extractNumArray(T.PW_Tx5_B2);
+
+T.Duration_Tx1_B1 = extractNumArray(T.Duration_Tx1_B1);
+T.Duration_Tx1_B2 = extractNumArray(T.Duration_Tx1_B2);
+T.Duration_Tx2_B1 = extractNumArray(T.Duration_Tx2_B1);
+T.Duration_Tx2_B2 = extractNumArray(T.Duration_Tx2_B2);
+T.Duration_Tx3_B1 = extractNumArray(T.Duration_Tx3_B1);
+T.Duration_Tx3_B2 = extractNumArray(T.Duration_Tx3_B2);
+T.Duration_Tx4_B1 = extractNumArray(T.Duration_Tx4_B1);
+T.Duration_Tx4_B2 = extractNumArray(T.Duration_Tx4_B2);
+T.Duration_Tx5_B1 = extractNumArray(T.Duration_Tx5_B1);
+T.Duration_Tx5_B2 = extractNumArray(T.Duration_Tx5_B2);
+
+T.CD_Tx1_B1 = extractNumArray(T.CD_Tx1_B1);
+T.CD_Tx1_B2 = extractNumArray(T.CD_Tx1_B2);
+T.CD_Tx2_B1 = extractNumArray(T.CD_Tx2_B1);
+T.CD_Tx2_B2 = extractNumArray(T.CD_Tx2_B2);
+T.CD_Tx3_B1 = extractNumArray(T.CD_Tx3_B1);
+T.CD_Tx3_B2 = extractNumArray(T.CD_Tx3_B2);
+T.CD_Tx4_B1 = extractNumArray(T.CD_Tx4_B1);
+T.CD_Tx4_B2 = extractNumArray(T.CD_Tx4_B2);
+T.CD_Tx5_B1 = extractNumArray(T.CD_Tx5_B1);
+T.CD_Tx5_B2 = extractNumArray(T.CD_Tx5_B2);
+
+PDMS_data = T;
+save(fullfile(pth, [outputFileName, '.mat']), 'PDMS_data');
+disp(['PDMS parsed and saved at', fullfile(pth, [outputFileName, '.mat'])])
+
+%%
+
+function parsed= parseLeads(leadstrs)
+
+for i_ld = 1:length(leadstrs)
+    if strcmp(leadstrs{i_ld}, 'OFF')
+        parsed(i_ld,:) = zeros(1,9);
+    else
+        a= replace(leadstrs{i_ld}, '-', '-1,');
+        b= replace(a, '0', '0,');
+        c= replace(b, '+', '1,');
+        d= strsplit(c,{'(', ')'});
+        p= [str2num(d{2}), str2num(d{3}), str2num(d{4})];
+        
+        parsed(i_ld,:) = p;
+    end
+end
+end
+
+function parsed = extractNumArray(numCells)
+    parsed= zeros(length(numCells), 1);
+    i_on= ~strcmp(numCells, 'OFF');
+    nums= cellfun(@str2num, regexp([numCells{:}],'\d+\.?\d*','match')');
+    parsed(i_on)= nums;
+
+end
+end
--- scripts/process_raw.m
+++ scripts/process_raw.m
@@ -3,11 +3,11 @@
 % load configuration settings
 %cd '/Volumes/data/RNS_DataSharing/RNS_Processing_Toolbox'
 
-config= jsondecode(fileread('../config_mounted.JSON')); 
+config= jsondecode(fileread('./config.JSON')); 
 nPts = length(config.patients); 
 
 % add toolboxes
-addpath(genpath('../conversion'))
+addpath(genpath('./conversion'))
 
 % ------------------------------------------------------
 % Convert .DAT files to .MAT files, place in MAT_Folder
@@ -27,10 +27,16 @@ for i_pt = 1:nPts
    dataFolder = fullfile(config.paths.DAT_Folder, [prefix, ' EXTERNAL #PHI'], [prefix, ' Data EXTERNAL #PHI']);
    catalog_csv= fullfile(config.paths.DAT_Folder, [prefix, ' EXTERNAL #PHI'], [prefix, '_ECoG_Catalog.csv']);
    
-   [AllData, AllTime]= dat2vector(dataFolder, catalog_csv); 
-    
-    
-    
+   %[AllData, AllTime]= dat2vector(dataFolder, catalog_csv);
+   
+   csv_file = readtable(catalog_csv);
+   TimeConversionMicroseconds = (posixtime(csv_file.RawUTCTimestamp)-posixtime(csv_file.RawLocalTimestamp))*10^6;
+   StartTimes = posixtime(csv_file.Timestamp)*10^6 + TimeConversionMicroseconds; % Convert to UTC
+   EcogLengths = csv_file.ECoGLength; % seconds
+   EndTimes = StartTimes + EcogLengths*10^6;
+       
+   save(fullfile(config.paths.MAT_Folder, sprintf('%s.mat', config.patients{i_pt}.RNS_ID)),...
+       'AllData', 'AllTime', 'StartTimes', 'EndTimes', 'TimeConversionMicroseconds')
     
 end
 
