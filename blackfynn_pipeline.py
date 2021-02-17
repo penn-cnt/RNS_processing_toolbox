6eb0b88731e9f4365da27c22fc97803587e0498c
Brittany Scheid <bscheid@seas.upenn.edu>
2021-02-17 11:27:58 -0500

    Update blackfynn pipeline, annotation upload works

--- blackfynn_pipeline.py
+++ blackfynn_pipeline.py
@@ -9,51 +9,22 @@ from rns_py_tools import NPDataHandler as npdh
 with open('./config.JSON') as f:
     config= json.load(f); 
 
-def uploadPatientAnnots(ptID_list):
+def uploadPatientCatalogAnnots(ptID, config):
 	''' Load annotations from ECoG Catalog for all patient indices in ptID_list '''
 
-	# Load data from config.JSON file 
-	f = open('config.JSON') 
-	data = json.load(f) 
-	f.close() 
-
-	inst = data['institution']
-	ptList = data['patients']
-	paths = data['paths']
+	bf_tools.annotate_from_catalog(ptID, config)
 
 
-	for i_pt in ptID_list:
-		prefix =  "_".join([inst, ptList[i_pt]['Initials'], ptList[i_pt]['PDMS_ID']])
-		package = ptList[i_pt]['bf_package']
-		ecog_catalog = os.path.join(paths['RNS_RAW_Folder'], prefix +' EXTERNAL #PHI', '_'.join([prefix, 'ECoG_Catalog.csv']))
+def pullPatientAnnots(config, layerName):
+    '''pull annotatios from layerName for all patients in ptID_list '''
 
-		print('Uploading annotations for patient %s'%ptList[i_pt]['Initials'])
+    ptList = [k['ID'] for k in config['patients']]
 
-		bf_tools.annotate_from_catalog(package, ecog_catalog)
-
-
-def pullPatientAnnots(ptID_list, layerName):
-	'''pull annotatios from layerName for all patients in ptID_list '''
-
-	# Load data from config.JSON file 
-	f = open('config.JSON') 
-	data = json.load(f) 
-	f.close() 
-
-	ptList = data['patients']
-	paths = data['paths']
-
-
-	for i_pt in ptID_list:
-		outputPath = os.path.join(paths['RNS_DATA_Folder'], ptList[i_pt]['RNS_ID'])
-		package = ptList[i_pt]['bf_package']
-		bf_tools.pull_annotations(package, layerName, outputPath)
+    for pt in ptList:
+        outputPath = os.path.join(config['paths']['RNS_DATA_Folder'], pt)
+        bf_tools.pull_annotations(pt, config, layerName, outputPath)
 		
-		print('Pulling annotations for patient %s'%ptList[i_pt]['RNS_ID'])
-
-
-# uploadPatientAnnots([16,5,3,12])
-# pullPatientAnnots(np.arange(0,19), 'BL_Annotation')
+        print('Pulling annotations for patient %s'%pt)
 
 
 def uploadNewPatient(ptID, config):
@@ -64,4 +35,11 @@ def uploadNewPatient(ptID, config):
     
     # Upload mefs to blackfynn
     bf_tools.uploadNewDat(tsName, ptID, config)
+    
+
+if __name__ == "__main__":
+   
+    with open('./config.JSON') as f:
+        config= json.load(f); 
+        
     
