6f6c6cf51e420649ee65082b60c3f7ef47648c95
Brittany Scheid <bscheid@seas.upenn.edu>
2022-01-14 12:54:43 -0500

    Updated function call to vis_event, added time offset option

--- matlab_tools/utils/loadRNSptData.m
+++ matlab_tools/utils/loadRNSptData.m
@@ -1,6 +1,20 @@
-function [ecogT, ecogD, stims, histT, pdms] = loadRNSptData(ptID, rns_config)
-    warning('off','all')    
+function [ecogT, ecogD, stims, histT, pdms] = loadRNSptData(ptID, rns_config, options)
+% [ecogT, ecogD, stims, histT, pdms] = loadRNSptData(ptID, rns_config, options)
+%  INPUT:
+%       ptID (string): patient ID
+%       rns_config (struct): rns config.JSON struct
+%       options: 
+%          - 'timeOffset' (true/false): sets all time to start on Jan 1st, 1970
+%
+    
+    arguments
+        ptID
+        rns_config
+        options.timeOffset logical = false
+    end
 
+    warning('off','all')
+   
     ecogT = readtable(ptPth(ptID, rns_config, 'ecog catalog'));
     ecogD = matfile(ptPth(ptID, rns_config, 'device data'));
     stims = load(ptPth(ptID, rns_config, 'device stim'));
@@ -15,7 +29,20 @@ function [ecogT, ecogD, stims, histT, pdms] = loadRNSptData(ptID, rns_config)
         pdms = [];
     end
     
-    
-
+    if options.timeOffset
+        MINDATE = min([pdms.Programming_Date; ecogT.Timestamp; ecogT.RawLocalTimestamp;...
+            ecogT.RawUTCTimestamp; histT.RegionStartTime; histT.UTCStartTime]);
+        OFFSET = MINDATE - datetime(0, 'convertFrom', 'posixtime');
+        
+        % TimeShift all using OFFSET 
+        pdms.Programming_Date = pdms.Programming_Date - OFFSET;
+        ecogT.Timestamp = ecogT.Timestamp - OFFSET;
+        ecogT.RawLocalTimestamp = ecogT.RawLocalTimestamp - OFFSET;
+        ecogT.RawUTCTimestamp = ecogT.RawUTCTimestamp - OFFSET;
+        histT.RegionStartTime = histT.RegionStartTime - OFFSET;
+        histT.UTCStartTime = histT.UTCStartTime - OFFSET; 
+        stims.StimStartStopTimes = stims.StimStartStopTimes - OFFSET;
+        
+    end
         
 end
