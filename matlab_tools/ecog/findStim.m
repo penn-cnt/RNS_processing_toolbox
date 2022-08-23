01f51c902044f03c60ef88a1de5f5e9afaf99dbe
Brittany Scheid <bscheid@seas.upenn.edu>
2022-08-23 16:03:39 -0400

    Matlab updates on main

--- matlab_tools/ecog/findStim.m
+++ matlab_tools/ecog/findStim.m
@@ -1,4 +1,4 @@
-function [StimStartStopIndex, stats] = findStim(AllData, Min, Channel, show)
+function [StimStartStopIndex, stats] = findStim(AllData, ecogT, Min, show)
 % findStim finds the Stimulation Group periods in Neuropace RNS timeseries data
 %
 %   To use default Channel=1, Min=15, show = false
@@ -34,13 +34,14 @@ function [StimStartStopIndex, stats] = findStim(AllData, Min, Channel, show)
 %   Arjun Ravi Shankar
 %   Litt Lab July 2018
 %
-%   Updated Brittany Scheid (bscheid@seas.upenn.edu) May 2022
+%   Updated Brittany Scheid (bscheid@seas.upenn.edu) & Kenji Anon Aug 2022
+%   
 
 %% Variable Input Defaults
 arguments
     AllData
+    ecogT
     Min = 15     % min # of consecutive 0 slope points to make up Stimulation
-    Channel = 1  % channel to make detections on
     show = false
 end
 
@@ -53,22 +54,27 @@ end
 %% Find Stimulation Triggers
 
 %Find Slope of Data
-Slope=diff(AllData,1,2); %./4000;
+Slope = diff(AllData,1,2); %./4000;
+Slope(:, ecogT.EventStartIdx) = 1;
 
 %Correct for max and min flatlines and analog to digital conversion
 %artifacts: Set slope to 1 if hits lower or upper rails for < "Min" samples
+
+SSI_all = []; 
+
+for Channel = 1:4
 
 Slope(Channel, runInds(AllData(Channel,:)<100, Min)) = 1;
 Slope(Channel, runInds(AllData(Channel,:)>900, Min)) = 1;
 
 %Find Start and End Locations of Regions with Zero Slope 
 ZeroSlopeInflections=diff(Slope(Channel,:)==0);
-ZeroSlopeStarts=find(ZeroSlopeInflections==1)+1;
-ZeroSlopeEnds=find(ZeroSlopeInflections==-1)+1;
+ZeroSlopeStarts = find(ZeroSlopeInflections==1)+1;
+ZeroSlopeEnds = find(ZeroSlopeInflections==-1)+1;
 %%
 
 % If more starts then ends, ignore last one
-if find(ZeroSlopeInflections== -1,1, 'last') < find(ZeroSlopeInflections== 1,1, 'last')
+if ZeroSlopeEnds(end) < ZeroSlopeStarts(end)
     ZeroSlopeStarts = ZeroSlopeStarts(1:end-1);
 end
 
@@ -76,15 +82,42 @@ end
 SSI_start = ZeroSlopeStarts(ZeroSlopeEnds-ZeroSlopeStarts>=Min)';
 SSI_end = ZeroSlopeEnds(ZeroSlopeEnds-ZeroSlopeStarts>=Min)';
 
-%Correct for Double Stimulation or low-frequency stim conditions
+SSI_all = [SSI_all;...
+    [SSI_start, ones(length(SSI_start),1)];...
+    [SSI_end, -1*ones(length(SSI_end),1)]];
+
+fprintf('%d/4 ',Channel)
+end
+
+% Get union of stims across at least 3 channels:
+srted = sortrows(SSI_all, 1);
+ivl_count = cumsum(srted(:,2));
+is2 = find(ivl_count == 2); 
+
+i_ivl_starts = is2(ivl_count(is2+1)==3);
+i_ivl_ends = is2(ivl_count(is2-1)==3);
+
+SSI_start = srted(i_ivl_starts,1);
+SSI_end = srted(i_ivl_ends,1);
+
+fprintf('\n')
+
+
+% Correct for Double Stimulation or low-frequency stim conditions
+% Don't merge if start and ends are on different sides of a start
+% index
+
 StimGap= SSI_start(2:end)-SSI_end(1:end-1);
-i_gp = find(StimGap<100);  
+
+incl = filterWindows([SSI_end(1:end-1),SSI_start(2:end)], ecogT.EventStartIdx);
+i_gp = find(StimGap<100 & ~incl);
 SSI_start(i_gp+1)= []; 
 SSI_end(i_gp)= []; 
 
+
 StimStartStopIndex = [SSI_start, SSI_end];
 
- 
+
 % %Count Number of Stimulation Groups
 % 
 % % First Assume Every Stimulation is a Single Stim
@@ -146,13 +179,14 @@ end
 %%
 function indOut = runInds(q,maxRun)
     % given a binary input vector, returns a binary mask with 1 at
-    % locations of consecutive runs less than "maxRun" in length
+    % locations of consecutive runs of ones less than "maxRun" in length
 
-    s=diff(diff(cumsum([0,0,q,0,0])));
-    i_end = find(s == -1);
-    i_beg = find(s==1);
-    gp_sz = i_end - i_beg; 
-    
+    s = diff([0,q,0]);
+    iboth = find(abs(s) == 1);
+    i_beg = iboth(1:2:end);
+    i_end = iboth(2:2:end);
+    gp_sz =  i_end - i_beg;
+   
     indOut = false(1, length(q));
     for i_g = find(gp_sz < maxRun)
         indOut(i_beg(i_g):i_end(i_g)-1)= true; 
